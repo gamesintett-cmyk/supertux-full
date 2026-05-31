b883a884f79d3fb4aec842a32258a2e6717a8909
gamesintett-cmyk <gamesintett@gmail.com>
2026-05-30 21:05:12 -0500

    FloatingText 60s test

--- src/object/floating_text.cpp
+++ src/object/floating_text.cpp
@@ -35,7 +35,7 @@ FloatingText::FloatingText(const Vector& pos, int score) :
   text(std::to_string(score)),
   timer()
 {
-  timer.start(15.0f);
+  timer.start(60.0f);
 
   position.x -= static_cast<float>(text.size()) * 8.0f;
 }
@@ -49,7 +49,7 @@ FloatingText::update(float dt_sec)
     remove_me();
 }
 
-const float FADING_TIME = 2.0f;
+const float FADING_TIME = 10.0f;
 
 void
 FloatingText::draw(DrawingContext& context)
